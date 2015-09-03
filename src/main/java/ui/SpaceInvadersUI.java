72f4d8eb0063ac3db16154f36494a51f7b11c3d3
jochem725 <jochemlugtenburg@gmail.com>
2015-09-03 12:49:07 +0200

    Added SpaceInvadersUI class to load User Interface Teh SpaceInvadersUI object is used to maintain the User Interface. It can be used to load different fxml file views.

---
+++ src/main/java/ui/SpaceInvadersUI.java
@@ -0,0 +1,66 @@
+package ui;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Class which creates an object that is used to maintain
+ * the User Interface.
+ * @author Jochem
+ *
+ */
+public class SpaceInvadersUI {
+	
+	/**
+	 * Stage object, defining the primary Stage of the program.
+	 */
+	private Stage primaryStage;
+
+	/**
+	 * Constructor for a SpaceInvadersUI object.
+	 * @param stage A JavaFX Stage.
+	 */
+	public SpaceInvadersUI(final Stage stage) {
+		// Construction the object, setting the title of the application.
+		this.primaryStage = stage;
+		primaryStage.setTitle("Space Invaders");
+
+		// Loading the root layout.
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("SpaceInvadersUI.fxml"));
+			Scene scene = new Scene(root);
+			
+			primaryStage.setScene(scene);			
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * Load a User Interface fxml file into the root layout.
+	 * @param fxmlFile Filename of the FXML file containing the layout.
+	 */
+	public final void loadUIScreen(final String fxmlFile) {
+		try {
+			Parent screen = FXMLLoader.load(getClass().getResource(fxmlFile));
+			primaryStage.getScene().setRoot(screen);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * Launch the User Interface.
+	 */
+	public final void launch() {
+		primaryStage.show();
+	}
+
+}
