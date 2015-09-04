cca1e524b93fd9e8089e5e3d977c178601eb5adb
tcmdekoning <tcmdekoning@student.tudelft.nl>
2015-09-04 14:53:39 +0200

    Prepare for key handling

--- src/main/java/group22/space_invaders/Game.java
+++ src/main/java/group22/space_invaders/Game.java
@@ -1,4 +1,7 @@
 package group22.space_invaders;
+
+import javafx.scene.input.KeyCode;
+import java.util.ArrayList;
 /**
  * 
  * @author Dorian
@@ -22,27 +25,38 @@ public class Game {
 	/**
 	 * Starts the game.
 	 */
-	public void start() {
+	public final void start() {
 		inProgress = true;
 	}
 	/**
 	 * Pauses the game.
 	 */
-	public void stop() {
+	public final void stop() {
 		inProgress = false;
 	}
 	/**
 	 * Returns true if the game is in progress.
 	 * @return boolean if the game is in progress
 	 */
-	public boolean isInProgress() {
+	public final boolean isInProgress() {
 		return inProgress;
 	}
 	/**
 	 * Will update all the objects in the game.
+	 * @param pressedKeys the keys pressed since last tick
 	 */
-	public void tick() {
-		
+	public void tick(ArrayList<KeyCode> pressedKeys) {
+		if (pressedKeys.size() != 0) {
+			if (pressedKeys.contains(KeyCode.SPACE)) {
+				//TODO Shoot bullet from spaceship
+			}
+			if (pressedKeys.contains(KeyCode.A)) {
+				//TODO Decrease velX for the Spacship (move Left).
+			}
+			if (pressedKeys.contains(KeyCode.D)) {
+				//TODO Increase velX for the spaceship (move right).
+			}
+		}
 	}
 	/**
 	 * Returns the highscore.
@@ -53,10 +67,10 @@ public class Game {
 	}
 	/**
 	 * Sets highscore.
-	 * @param new highscore (int)
+	 * @param newscore highscore (int)
 	 * @throws IlligalArgumentException if the new highscore is less then the old highscore or highscore is negative.
 	 */
-	public void setHighScore(int newscore) {
+	public final void setHighScore(final mint newscore) {
 		assert newscore >= 0 && newscore > highscore;
 		highscore = newscore;
 	}
