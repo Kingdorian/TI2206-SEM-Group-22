9536f7b01a4ac20c7ebcdbded7f5818ceef0f88c
tcmdekoning <tcmdekoning@student.tudelft.nl>
2015-09-05 14:17:16 +0200

    Add score to the player object

--- src/main/java/spaceinvaders/group_22/Player.java
+++ src/main/java/spaceinvaders/group_22/Player.java
@@ -13,14 +13,21 @@ public class Player {
 	 * Spaceship the player is currently controling.
 	 */
 	private SpaceShip ship;
+	/**
+	 * Int that keeps track of the score of the player.
+	 */
+	private int score;
+	
+	//TODO Add Lives
 	
 	/**
-	 * Creates new spaceship object.
+	 * Creates new Player object.
 	 * @param game the game where the object is part of.
 	 */
 	@SuppressWarnings("checkstyle:magicnumber")
 	public Player(Game game) {
 		ship = new SpaceShip(10.0f, 10.0f);
+		score  = 0;
 	}
 	
 	/**
@@ -36,6 +43,26 @@ public class Player {
 	 */
 	public final void setSpaceShip(SpaceShip newShip) {
 		ship = newShip;
+	}
+	/**
+	 * Gets the players score.
+	 * @return the amount of points the player has scored.
+	 */
+	public final int getScore() {
+		return score;
+	}
+	/**
+	 * Adds an amount of points to the players score.
+	 * @param score the amount of points to add
+	 */
+	public final void addScore(int score) {
+		this.score+=score;
+	}
+	/**
+	 * Resets the amount of points the player has.
+	 */
+	public final void resetScore() {
+		score = 0;
 	}
 	
 	
--- src/test/java/spaceinvaders/group_22/PlayerTest.java
+++ src/test/java/spaceinvaders/group_22/PlayerTest.java
@@ -12,10 +12,37 @@ public class PlayerTest {
 	/**
 	 * Test behavior of the getSpaceShip method for the player.
 	 */
+	@Test
 	public void testGetSetSpaceShip() {
 		SpaceShip ship = new SpaceShip(10.0f, 10.0f);
 		Player player = new Player(new Game());
 		player.setSpaceShip(ship);
 		Assert.assertEquals(ship, player.getSpaceShip());
+	}
+	/**
+	 * Test behaviour of the getScore method for the player class.
+	 */
+	@Test
+	public void testGetScore () {
+		Player player = new Player(new Game());
+		Assert.assertEquals(0, player.getScore());
+	}
+	/**
+	 * Tests the behaviour of the addpoints method for the player class for positive amounts of points.
+	 */
+	@Test
+	public void testAddPositiveScore () {
+		Player player = new Player(new Game());
+		player.addScore(10);
+		Assert.assertEquals(10, player.getScore());
+	}
+	/**
+	 * Tests the behaviour of the addPoints method when adding a negative amount of points to the players score.
+	 */
+	@Test
+	public void testAddNegativeScore () { 
+		Player player = new Player(new Game());
+		player.addScore(-10);
+		Assert.assertEquals(-10, player.getScore());
 	}
 }
