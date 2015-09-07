2cc5c386e28fa69329dbcf0a88cd0fe2e84f52b7
jochem725 <jochemlugtenburg@gmail.com>
2015-09-07 13:55:40 +0200

    Removed unused import

--- src/main/java/spaceinvaders/group_22/ui/GameUIController.java
+++ src/main/java/spaceinvaders/group_22/ui/GameUIController.java
@@ -23,7 +23,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 /**
