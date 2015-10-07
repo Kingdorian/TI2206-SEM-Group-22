c1bc2255b22e3a58a58777d2f0688c4141c2da77
jochem725 <jochemlugtenburg@gmail.com>
2015-10-07 11:31:19 +0200

    Refactor powerup creation to AlienUnitFactory <organisation

--- src/main/java/spaceinvaders/group_22/unit/AbstractUnitFactory.java
+++ src/main/java/spaceinvaders/group_22/unit/AbstractUnitFactory.java
@@ -55,34 +55,4 @@ public abstract class AbstractUnitFactory {
 		return new Explosion(x, y, "explosion.png");
 	}
 	
-	/**
-	 * A method responsible for creating a SpeedPowerUpUnit.
-	 * @param x X Coordinate.
-	 * @param y Y Coordinate.
-	 * @return A SpeedPowerUpUnit object.
-	 */
-	public final SpeedPowerUpUnit createSpeedPowerup(final double x, final double y) {
-		return new SpeedPowerUpUnit(x, y, "powerup_blue.png");
-	}
-	
-	/**
-	 * A method responsible for creating a ShootPowerUpUnit.
-	 * @param x X Coordinate.
-	 * @param y Y Coordinate.
-	 * @return A ShootPowerUpUnit object.
-	 */
-	public final ShootPowerUpUnit createShootPowerup(final double x, final double y) {
-		return new ShootPowerUpUnit(x, y, "powerup_orange.png");
-	}
-	
-	/**
-	 * A method responsible for creating a LifePowerUpUnit.
-	 * @param x X Coordinate.
-	 * @param y Y Coordinate.
-	 * @return A LifePowerUpUnit object.
-	 */
-	public final LifePowerUpUnit createLifePowerup(final double x, final double y) {
-		return new LifePowerUpUnit(x, y, "powerup_red.png");
-	}
-	
 }
