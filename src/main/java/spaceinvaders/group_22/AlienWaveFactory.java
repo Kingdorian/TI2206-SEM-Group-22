6cf3eb3f8aefbf33f20ade0cf564659f83178340
unknown <doriandekoning@hotmail.com>
2015-10-08 16:30:00 +0200

    Add wavePattern to AwFactory and add first wave
    <feature

--- src/main/java/spaceinvaders/group_22/AlienWaveFactory.java
+++ src/main/java/spaceinvaders/group_22/AlienWaveFactory.java
@@ -52,18 +52,23 @@ public class AlienWaveFactory implements AlienWaveFactoryInterface {
 	}
 
 	@Override
-	public final AlienWave createWaves() {
+	public final ArrayList<AlienWave> createWaves() {
 		String sep = System.getProperty("file.separator");
+		ArrayList<AlienWave> waves = new ArrayList<AlienWave>();
 		try {
-			waveReader.read("src" + sep	+ "main" + sep + "resources" + sep
+			ArrayList<WavePattern> wavePatterns = 
+					waveReader.read("src" + sep	+ "main" + sep + "resources" + sep
 					+ "spaceinvaders" +  sep + "group_22" + sep	+ "waves" + sep);
+			for(WavePattern w : wavePatterns) {
+				waves.add(createWaveFromPattern(w));
+			}
 		} catch (IOException e) {
 			Game.getLogger().log("IOException occured while reading alienwaves", e);
-			return createDefaultWaves();
+			waves.add(createDefaultWave());
 		}
-		return patterns.get((int) Math.random() * patterns.size()).copy();	
+		return waves;
 	}
-	public final AlienWave createDefaultWaves() {
+	public final AlienWave createDefaultWave() {
 		ConcreteAlienWave wave = new ConcreteAlienWave();
 		ArrayList<Alien> aliens = new ArrayList<Alien>();
 		
@@ -88,5 +93,35 @@ public class AlienWaveFactory implements AlienWaveFactoryInterface {
         }
         return wave;
 	}
-
+	/**
+	 * Creates a new alienwave according to the supplied pattern
+	 * @return pattern to create a alienwave from
+	 */
+	public final AlienWave createWaveFromPattern(WavePattern pattern) {
+		ConcreteAlienWave wave = new ConcreteAlienWave();
+		ArrayList<Alien> aliens = new ArrayList<Alien>();
+		
+		 // Distance to top of the screen.
+        double y = 125;
+        // Create alien object to make sure we can get the width and height of aliens
+        Alien testAlien = new Alien(0.0, 0.0, Alien.SPRITE); 
+        // Drawing lines of Aliens.
+        for (int i = 0; i < pattern.getHeight(); i++) {
+        	 double interval = ((game.getCanvasWidth() - (2 * ALIENBORDERMARIGIN * game.getCanvasWidth()))
+						- (pattern.getLength(i) * testAlien.getWidth())) / (pattern.getLength(i) + 1);  
+            double x = ALIENBORDERMARIGIN * game.getCanvasWidth() + 0.5 * testAlien.getWidth();
+            for (int j = 0; j < pattern.getLength(i); j++) {
+            	switch (pattern.getChar(i, j)) {
+            		case '*':
+            			Alien alien = new Alien(x, y, "invader.png");
+                    	alien.setVelX(alienVelX);
+                    	aliens.add(alien);
+                      	Game.getLogger().log("Created Alien at location:(" + x + "," + y + ")", LogEvent.Type.TRACE);
+            	}
+            	x += testAlien.getWidth() + interval;
+            }
+            y += 1.1 * testAlien.getHeight();
+        }
+        return wave;
+	}
 }
--- src/main/java/spaceinvaders/group_22/AlienWaveFactoryInterface.java
+++ src/main/java/spaceinvaders/group_22/AlienWaveFactoryInterface.java
@@ -1,4 +1,7 @@
 package spaceinvaders.group_22;
+
+import java.util.ArrayList;
+
 /**
  * Interface for creating an alienWave.
  * @author Dorian and Bryan
@@ -7,8 +10,18 @@ package spaceinvaders.group_22;
 
 public interface AlienWaveFactoryInterface {
 	/**
-	 * Method to create a new alienWave.
+	 * Method to create a list of alienwaves.
 	 * @return new Alienwave
 	 */
-	AlienWave createWaves();
+	ArrayList<AlienWave> createWaves();
+	/**
+	 * Method to create a default alienwave.
+	 * @return new default alienwave Alienwave
+	 */
+	AlienWave createDefaultWave();
+	/**
+	 * Method to create a default alienwave.
+	 * @return new default alienwave Alienwave
+	 */
+	AlienWave createWaveFromPattern(WavePattern w);
 }
