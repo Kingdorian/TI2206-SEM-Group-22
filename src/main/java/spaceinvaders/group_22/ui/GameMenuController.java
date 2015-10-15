6a9de2795d7c57f7510302dff79945b7359549a4
unknown <doriandekoning@hotmail.com>
2015-10-15 13:36:13 +0200

    Fix not loading game screens properly
    <feature, interface

--- src/main/java/spaceinvaders/group_22/ui/GameMenuController.java
+++ src/main/java/spaceinvaders/group_22/ui/GameMenuController.java
@@ -6,11 +6,11 @@ import spaceinvaders.group_22.logger.Logger;
 public class GameMenuController {
 	
 	public void startSinglePlayer() {
-		SpaceInvadersUI.getInstance().loadUIScreen("SinglePlayerGame.fxml");
+		SpaceInvadersUI.getInstance().loadUIScreen("SinglePlayerGameUI.fxml");
 		Logger.getInstance().log("Starting a singleplayer game", LogEvent.Type.DEBUG);
 	}
 	public void startMultiPlayer() {
-		SpaceInvadersUI.getInstance().loadUIScreen("SinglePlayerGame.fxml");
+		SpaceInvadersUI.getInstance().loadUIScreen("MultiPlayerGameUI.fxml");
 		Logger.getInstance().log("Starting a multiplayer game", LogEvent.Type.DEBUG);
 	}
 	public void quit() {
