f53370abd485ef945f42e332ab9efba6fe7e60bf
unknown <doriandekoning@hotmail.com>
2015-10-16 09:57:13 +0200

    Fix live positions in MultiPlayer
    <bug, interface

--- src/main/java/spaceinvaders/group_22/MultiPlayerGameUI/Lives.java
+++ src/main/java/spaceinvaders/group_22/MultiPlayerGameUI/Lives.java
@@ -32,15 +32,14 @@ public class Lives extends spaceinvaders.group_22.ui.UIElementLives {
 		Image heartImage = SpriteLoader.getInstance().getHeart();
 		//Change lives for player 1
 		for (int i = 1; i <= ((MultiPlayerGame)getGame()).getPlayers().get(0).getLives(); i++) {
-			Logger.getInstance().log("Drawing player 1 lives",  LogEvent.Type.DEBUG);
-        	getGC().drawImage(heartImage, getGame().getCanvasWidth() - 10 - heartImage.getWidth() * i, 40);
+			Logger.getInstance().log("Drawing player 2 lives",  LogEvent.Type.DEBUG);
+        	getGC().drawImage(heartImage, 10 + (heartImage.getWidth() * (i - 1)), 70);
     	}
     	// Draw lives for player 2
 		for (int i = 1; i <= ((MultiPlayerGame)getGame()).getPlayers().get(1).getLives(); i++) {
-			Logger.getInstance().log("Drawing player 2 lives",  LogEvent.Type.DEBUG);
-        	getGC().drawImage(heartImage,  10 + heartImage.getWidth() * i, 40);
+			Logger.getInstance().log("Drawing player 1 lives",  LogEvent.Type.DEBUG);
+        	getGC().drawImage(heartImage, (getGame().getCanvasWidth() - 10) - heartImage.getWidth() * i, 70);
     	}
-    	
     	Logger.getInstance().log("Formatted hearts to UI", LogEvent.Type.TRACE);
 	}
 
