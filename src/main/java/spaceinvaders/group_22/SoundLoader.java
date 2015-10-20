cc1fe5219802c527f705f001c90de1ee292fcb0d
jochem725 <jochemlugtenburg@gmail.com>
2015-10-20 13:02:00 +0200

    Add Loading and playback of sound files <feature

--- src/main/java/spaceinvaders/group_22/SoundLoader.java
+++ src/main/java/spaceinvaders/group_22/SoundLoader.java
@@ -1,5 +1,9 @@
 package spaceinvaders.group_22;
 
+import java.util.HashMap;
+
+import javafx.scene.media.AudioClip;
+import spaceinvaders.group_22.logger.LogEvent;
 import spaceinvaders.group_22.logger.Logger;
 
 /**
@@ -15,10 +19,34 @@ public final class SoundLoader {
 	private static volatile SoundLoader uniqueInstance;
 	
 	/**
-	 * Constructor for a SoundLoader.
+	 * The path to the sound folder.
 	 */
-	private SoundLoader() {
-		
+	private String soundFolderPath;
+	
+	/**
+	 * Hashmap containing all audio fragments.
+	 */
+	private HashMap<String, AudioClip> sounds;
+	
+	/**
+	 * Constructor for a SoundLoader.
+	 * @param sFP the path to the sound folder.
+	 */
+	private SoundLoader(final String sFP) {
+		soundFolderPath = sFP;
+    	sounds = new HashMap<String, AudioClip>();
+    	
+    	addSoundFile("end.wav");
+    	addSoundFile("explosion.wav");
+    	addSoundFile("powerup_life.wav");
+    	addSoundFile("powerup_shoot.wav");
+    	addSoundFile("powerup_speed.wav");
+    	addSoundFile("shoot.wav");
+    	addSoundFile("start.wav");
+    	
+    	sounds.get("start.wav").play();
+    	
+    	Logger.getInstance().log("Initialized " + getClass().getName(), LogEvent.Type.INFO);
 	}
 	
 	/**
@@ -29,11 +57,71 @@ public final class SoundLoader {
 		if (uniqueInstance == null) {
 			synchronized (SoundLoader.class) {
 				if (uniqueInstance == null) {
-					uniqueInstance = new SoundLoader();
+					uniqueInstance = new SoundLoader("spaceinvaders/group_22/sound/");
 		       }
 			}	
 		}
 		return uniqueInstance; 
 	}
+	
+	/**
+	 * Method to load and add sound files to the sounds hashmap.
+	 * @param filename A sound filename.
+	 */
+	private void addSoundFile(final String filename) {
+		sounds.put(filename, 
+				new AudioClip(getClass().getClassLoader()
+						.getResource(soundFolderPath + filename).toString()));
+		Logger.getInstance().log("Loaded Sound " + filename, LogEvent.Type.DEBUG);
+	}
+	
+    /**
+     * Getter method for the end game sound.
+     * @return An AudioClip.
+     */
+    public AudioClip getEndGame() {
+    	return sounds.get("end.wav");
+    }
+    
+    /**
+     * Getter method for an explosion sound.
+     * @return An AudioClip.
+     */
+    public AudioClip getExplosion() {
+    	return sounds.get("explosion.wav");
+    }
+    
+    /**
+     * Getter method for a Life powerup sound.
+     * @return An AudioClip.
+     */
+    public AudioClip getLifePowerUp() {
+    	return sounds.get("powerup_life.wav");
+    }
+    
+    /**
+     * Getter method for a Speed powerup sound.
+     * @return An AudioClip.
+     */
+    public AudioClip getSpeedPowerUp() {
+    	return sounds.get("powerup_speed.wav");
+    }
+    
+    /**
+     * Getter method for a Shoot powerup sound.
+     * @return An AudioClip.
+     */
+    public AudioClip getShootPowerUp() {
+    	return sounds.get("powerup_shoot.wav");
+    }
+
+    /**
+     * Getter method for the start game sound.
+     * @return An AudioClip.
+     */
+    public AudioClip getStartGame() {
+    	return sounds.get("start.wav");
+    }
+    
 
 }
