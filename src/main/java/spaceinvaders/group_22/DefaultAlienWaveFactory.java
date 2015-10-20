294f6166b2b9101a4331c10f27dbfa10a52d1efc
Ege de Bruin <kedebruin@student.tudelft.nl>
2015-10-20 23:13:28 +0200

    Added unimplemented methods

--- src/main/java/spaceinvaders/group_22/DefaultAlienWaveFactory.java
+++ src/main/java/spaceinvaders/group_22/DefaultAlienWaveFactory.java
@@ -59,4 +59,9 @@ public class DefaultAlienWaveFactory implements AlienWaveFactoryInterface {
         
         return wave;
 	}
+	@Override
+	public AlienWave createBossWave() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
