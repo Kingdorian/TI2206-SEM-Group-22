24d18ee980d53416e83c9bb4a34acad25176be0c
jochem725 <jochemlugtenburg@gmail.com>
2015-10-21 13:43:18 +0200

    Add sound and background music booleans <feature

--- src/main/java/spaceinvaders/group_22/sound/SoundController.java
+++ src/main/java/spaceinvaders/group_22/sound/SoundController.java
@@ -1,7 +1,7 @@
 package spaceinvaders.group_22.sound;
 
-
 import javafx.animation.Transition;
+import javafx.scene.media.AudioClip;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 import spaceinvaders.group_22.Observer;
@@ -24,40 +24,59 @@ public final class SoundController implements Observer {
 		 * Start game sound.
 		 */
 		START_GAME {
+				/**
+				 * AudioClip for this ENUM.
+				 */
+				private AudioClip clip = SoundLoader.getInstance().getStartGame();
+				
 				@Override
 				public void play() { 
-					SoundLoader.getInstance().getStartGame().play(); 
+					playSFX(clip); 
 				} 
+				
 				@Override
 				public void stop() {
-					SoundLoader.getInstance().getStartGame().stop(); 
+					clip.stop(); 
 				} },
 		
 		/**
 		 * Stop game sound.
 		 */
-		STOP_GAME { 
+		STOP_GAME {
+				/**
+				 * AudioClip for this ENUM.
+				 */
+				private AudioClip clip = SoundLoader.getInstance().getEndGame();	
+					
+					
 				@Override
 				public void play() { 
-					SoundLoader.getInstance().getEndGame().play(); 
+					playSFX(clip); 
 				} 
 				@Override
 				public void stop() {
-					SoundLoader.getInstance().getStartGame().stop(); 
+					clip.stop(); 
 				} },
 		/**
 		 * Sets the background music.
 		 */
 		BGM { 
+				/**
+				 * MediaPlayer for this ENUM.
+				 */
+				private MediaPlayer player = SoundLoader.getInstance().getBGMMusicPlayer();	
+					
+				/**
+				 * The fade of this ENUM.
+				 */
+				private static final double FADE = 8000;
+				
 				@Override
 				public void play() { 
-					MediaPlayer player = SoundLoader.getInstance().getBGMMusicPlayer();
-					player.setCycleCount(MediaPlayer.INDEFINITE);
-					musicFadeIn(player, 8000); 
+					playBGM(player, FADE);
 				} 
 				@Override
 				public void stop() {
-					MediaPlayer player = SoundLoader.getInstance().getBGMMusicPlayer();
 					player.stop();
 				} }; 
 		
@@ -71,6 +90,28 @@ public final class SoundController implements Observer {
 		 * Stops the ENUM value.
 		 */
 		public abstract void stop();
+		
+		/**
+		 * Plays the SFX if sfx is enabled.
+		 * @param clip The clip to play the music for.
+		 */
+		public void playSFX(final AudioClip clip) {
+			if (SoundController.getInstance().getSFXEnabled()) {
+				clip.play();
+			}
+		};
+		
+		/**
+		 * Plays the BGM if bgm is enabled.
+		 * @param player The musicPlayer to play the music for.
+		 * @param fade The time the fade should take to fade the backgroundmusic in.
+		 */
+		public void playBGM(final MediaPlayer player, final double fade) {
+			if (SoundController.getInstance().getBGMEnabled()) {
+				player.setCycleCount(MediaPlayer.INDEFINITE);
+				musicFadeIn(player, fade); 
+			}
+		};
 		
 		/**
 		 * Fades music on play.
@@ -113,6 +154,16 @@ public final class SoundController implements Observer {
 	private SoundController() { }
 	
 	/**
+	 * Defines if the background music is enabled.
+	 */
+	private boolean bgmEnabled = true;
+	
+	/**
+	 * Defines if the sound effects are enabled.
+	 */
+	private boolean sfxEnabled = true;
+	
+	/**
 	 * Returns the singleton instance of Logger.
 	 * @return the Loggers unique instance.
 	 */
@@ -132,6 +183,38 @@ public final class SoundController implements Observer {
 		if (o instanceof Soundable) {
 			((Soundable) o).getAudioClip().play();
 		}
+	}
+	
+	/**
+	 * Getter for the Sound effect boolean.
+	 * @return True if enabled, false else.
+	 */
+	public boolean getSFXEnabled() {
+		return sfxEnabled;
+	}
+	
+	/**
+	 * Sets if sound effects should be played.
+	 * @param b true if enabled, false else.
+	 */
+	public void setSFXEnabled(final Boolean b) {
+		sfxEnabled = b;
+	}
+	
+	/**
+	 * Getter for the Background music boolean.
+	 * @return True if enabled, false else.
+	 */
+	public boolean getBGMEnabled() {
+		return bgmEnabled;
+	}
+	
+	/**
+	 * Sets if background music should be played.
+	 * @param b true if enabled, false else.
+	 */
+	public void setBGMEnabled(final Boolean b) {
+		bgmEnabled = b;
 	}
 
 }
