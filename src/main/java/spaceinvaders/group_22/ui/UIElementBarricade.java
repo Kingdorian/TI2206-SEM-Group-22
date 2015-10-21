68e310d0aca2344a385a21fd3522371dfa691e5d
unknown <doriandekoning@hotmail.com>
2015-10-21 12:04:08 +0200

    Add drawing barricade damage

--- src/main/java/spaceinvaders/group_22/ui/UIElementBarricade.java
+++ src/main/java/spaceinvaders/group_22/ui/UIElementBarricade.java
@@ -1,8 +1,17 @@
 package spaceinvaders.group_22.ui;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import spaceinvaders.group_22.Game;
+import spaceinvaders.group_22.logger.LogEvent;
+import spaceinvaders.group_22.logger.Logger;
 import spaceinvaders.group_22.unit.Barricade;
+import spaceinvaders.group_22.unit.Unit;
 
 /**
  * The drawing of the barricades.
@@ -33,6 +42,39 @@ public class UIElementBarricade extends UIElementUnit {
 			getGC().setGlobalAlpha(1);
 		}
 		
+	}
+	@Override
+	public final void drawUnit(final Unit barricade) { 
+		Image spriteImage = barricade.getSprite();
+		if(spriteImage == null) {
+			Logger.getInstance().log("Error reading spriteImage barricade", LogEvent.Type.TRACE);
+			return;
+		}
+		Barricade bar = (Barricade)barricade;
+		final boolean[][] damage = bar.getDamage();
+
+		int width = (int)spriteImage.getWidth();
+		int height =  (int)spriteImage.getHeight();
+		PixelReader pixelReader = spriteImage.getPixelReader();
+		WritableImage finalSprite = 
+				new WritableImage(width, height);
+		PixelWriter pixelWriter = finalSprite.getPixelWriter();
+		double xInterval = bar.getWidth()/damage.length;
+		double yInterval = bar.getHeight()/damage[0].length;
+	    for (int y = 0; y < height; y++){
+            for (int x = 0; x < width; x++){
+            	Color color;
+            	if (damage[(int)(y/yInterval)][(int)(x/xInterval)]) {
+            		color = pixelReader.getColor(x, y);
+            	} else {
+            		color = new Color(0, 0, 0, 1.0);
+            	}
+            	pixelWriter.setColor(x, y, color);
+            }
+        }
+		getGC().drawImage(finalSprite, calculateXposition(bar), calculateYposition(bar));	
+			
+		Logger.getInstance().log("Drawn " + bar.getClass().getName(), LogEvent.Type.TRACE);
 	}
 
 }
